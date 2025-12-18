cf7956fc358f21d5867db9eeeaf96ccd810daf26
abhinavkatiyar333 <abhinavkatiyar727@gmail.com>
2025-12-18 11:38:03 +0530

    Search element in Matrix

---
+++ MatrixProblem/SearchElement.java
@@ -0,0 +1,19 @@
+
+
+public class SearchElement {
+          public static void main(String[] args) {
+                    int arr[][] = {{1,2,3},
+                                   {4,5,6},
+                                   {7,8,9}};
+                    int target = 7;
+
+                    for(int i=0;i<arr.length;i++){
+                         for(int j=0;j<arr[i].length;j++){
+                              if(arr[i][j]== target){
+                                      System.out.print(i+" "+j);  
+                              }
+                         }
+                    }             
+          }
+          
+}
